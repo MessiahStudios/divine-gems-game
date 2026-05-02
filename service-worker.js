c7e63387d40f1cfad85578450aad50146515681e
Kenneth James Mendoza <messiahstudiosphx@gmail.com>
2026-05-02 14:21:32 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,136 +1,131 @@
-// Cache version
-const CACHE_NAME = 'divine-gems-v1';
+// Bump version when precache list changes so old caches are dropped.
+const CACHE_NAME = 'divine-gems-v2';
 
-// Files to cache
-const FILES_TO_CACHE = [
-  '/divine-gems-game/',
-  '/divine-gems-game/index.html',
-  '/divine-gems-game/style.css',
-  '/divine-gems-game/bar.js',
-  '/divine-gems-game/counter.js',
-  '/divine-gems-game/favicon.ico', // Added this line
-  '/divine-gems-game/match3.js',
-  '/divine-gems-game/jquery-1.11.1.min.js',
-  '/divine-gems-game/wade.ifx_1.0.js',
-  '/divine-gems-game/wade.particles_1.0.1.js',
-  '/divine-gems-game/wade_1.5.js',
-  '/divine-gems-game/install.js',        // Add this line
-  // Include all the fonts
-  '/divine-gems-game/fonts/Akashi.eot',
-  '/divine-gems-game/fonts/Akashi.svg',
-  '/divine-gems-game/fonts/Akashi.ttf',
-  '/divine-gems-game/fonts/Akashi.woff',
-  '/divine-gems-game/fonts/MonospaceTypewriter.eot',
-  '/divine-gems-game/fonts/MonospaceTypewriter.svg',
-  '/divine-gems-game/fonts/MonospaceTypewriter.ttf',
-  '/divine-gems-game/fonts/MonospaceTypewriter.woff',
-  // Add all images
-  '/divine-gems-game/images/Thumbs.db',
-  '/divine-gems-game/images/background.png',
-  '/divine-gems-game/images/backgroundShareBox.png',
-  '/divine-gems-game/images/barTime.png',
-  '/divine-gems-game/images/bigBoom.png',
-  '/divine-gems-game/images/buttonBack.png',
-  '/divine-gems-game/images/buttonCredit.png',
-  '/divine-gems-game/images/buttonPause.png',
-  '/divine-gems-game/images/buttonPlay.png',
-  '/divine-gems-game/images/buttonSoundOff.png',
-  '/divine-gems-game/images/buttonSoundOn.png',
-  '/divine-gems-game/images/buttonUnpause.png',
-  '/divine-gems-game/images/buttonsMuteOff.png',
-  '/divine-gems-game/images/buttonsMuteOn.png',
-  '/divine-gems-game/images/christian_arabic_symbol_glow.png',
-  '/divine-gems-game/images/christian_arabic_symbol_new.png',
-  '/divine-gems-game/images/cross_object_glow.png',
-  '/divine-gems-game/images/cross_object_new.png',
-  '/divine-gems-game/images/crown_object_glow.png',
-  '/divine-gems-game/images/crown_object_new.png',
-  '/divine-gems-game/images/dgTitle.png',
-  '/divine-gems-game/images/divineGemsTitle.png',
-  '/divine-gems-game/images/divineGemsTitle_padded_192x192.png',
-  '/divine-gems-game/images/divineGemsTitle_padded_512x512.png',
-  '/divine-gems-game/images/fbSend.png',
-  '/divine-gems-game/images/fb_R.png',
-  '/divine-gems-game/images/fish_symbol_glow.png',
-  '/divine-gems-game/images/fish_symbol_new.png',
-  '/divine-gems-game/images/fiveEffect.png',
-  '/divine-gems-game/images/fiveEffect2.png',
-  '/divine-gems-game/images/flash.png',
-  '/divine-gems-game/images/gh_R.png',
-  '/divine-gems-game/images/imdb_R.png',
-  '/divine-gems-game/images/installButton.png',
-  '/divine-gems-game/images/inst_R.png',
-  '/divine-gems-game/images/li_R.png',
-  '/divine-gems-game/images/markerTime.png',
-  '/divine-gems-game/images/menuBackground.png',
-  '/divine-gems-game/images/nails_object_glow.png',
-  '/divine-gems-game/images/nails_object_new.png',
-  '/divine-gems-game/images/potionBar.png',
-  '/divine-gems-game/images/scoreArea.png',
-  '/divine-gems-game/images/scoreArea.xcf',
-  '/divine-gems-game/images/selected.png',
-  '/divine-gems-game/images/shatter.png',
-  '/divine-gems-game/images/special4.png',
-  '/divine-gems-game/images/special5-lion.png',
-  '/divine-gems-game/images/specialEffect1.png',
-  '/divine-gems-game/images/x_R.png',
-  '/divine-gems-game/images/top.png',
-  '/divine-gems-game/images/topWithScore.png',
-  '/divine-gems-game/images/trinity_object_glow.png',
-  '/divine-gems-game/images/trinity_object_new.png',
-  '/divine-gems-game/images/vlog_R.png',
-  '/divine-gems-game/images/wadePowered.png',
-  '/divine-gems-game/images/yt_R.png',
-  // Add all sound files
-  '/divine-gems-game/sounds/Explosion3.aac',
-  '/divine-gems-game/sounds/Explosion3.ogg',
-  '/divine-gems-game/sounds/PowerUp8.aac',
-  '/divine-gems-game/sounds/PowerUp8.ogg',
-  '/divine-gems-game/sounds/Walperion-Music-Ode-to-Victory.aac',
-  '/divine-gems-game/sounds/Walperion-Music-Ode-to-Victory.ogg',
-  '/divine-gems-game/sounds/fiveSound-lion.aac',
-  '/divine-gems-game/sounds/fiveSound-lion.ogg',
-  // Add additional files as necessary
-];
+self.addEventListener('install', function (event) {
+  var base = self.registration.scope;
+  function u(path) {
+    return new URL(path, base).href;
+  }
 
-// Install event - Caching static assets
-self.addEventListener('install', event => {
+  var filesToCache = [
+    u('index.html'),
+    u('app.js'),
+    u('manifest.json'),
+    u('style.css'),
+    u('bar.js'),
+    u('counter.js'),
+    u('favicon.ico'),
+    u('match3.js'),
+    u('jquery-1.11.1.min.js'),
+    u('wade.ifx_1.0.js'),
+    u('wade.particles_1.0.1.js'),
+    u('wade_1.5.js'),
+    u('install.js'),
+    u('fonts/Akashi.eot'),
+    u('fonts/Akashi.svg'),
+    u('fonts/Akashi.ttf'),
+    u('fonts/Akashi.woff'),
+    u('fonts/MonospaceTypewriter.eot'),
+    u('fonts/MonospaceTypewriter.svg'),
+    u('fonts/MonospaceTypewriter.ttf'),
+    u('fonts/MonospaceTypewriter.woff'),
+    u('images/background.png'),
+    u('images/backgroundShareBox.png'),
+    u('images/barTime.png'),
+    u('images/bigBoom.png'),
+    u('images/buttonBack.png'),
+    u('images/buttonCredit.png'),
+    u('images/buttonPause.png'),
+    u('images/buttonPlay.png'),
+    u('images/buttonSoundOff.png'),
+    u('images/buttonSoundOn.png'),
+    u('images/buttonUnpause.png'),
+    u('images/buttonsMuteOff.png'),
+    u('images/buttonsMuteOn.png'),
+    u('images/christian_arabic_symbol_glow.png'),
+    u('images/christian_arabic_symbol_new.png'),
+    u('images/cross_object_glow.png'),
+    u('images/cross_object_new.png'),
+    u('images/crown_object_glow.png'),
+    u('images/crown_object_new.png'),
+    u('images/dgTitle.png'),
+    u('images/divineGemsTitle.png'),
+    u('images/divineGemsTitle_padded_192x192.png'),
+    u('images/divineGemsTitle_padded_512x512.png'),
+    u('images/fbSend.png'),
+    u('images/fb_R.png'),
+    u('images/fish_symbol_glow.png'),
+    u('images/fish_symbol_new.png'),
+    u('images/fiveEffect.png'),
+    u('images/fiveEffect2.png'),
+    u('images/flash.png'),
+    u('images/gh_R.png'),
+    u('images/imdb_R.png'),
+    u('images/installButton.png'),
+    u('images/inst_R.png'),
+    u('images/li_R.png'),
+    u('images/markerTime.png'),
+    u('images/menuBackground.png'),
+    u('images/nails_object_glow.png'),
+    u('images/nails_object_new.png'),
+    u('images/potionBar.png'),
+    u('images/scoreArea.png'),
+    u('images/selected.png'),
+    u('images/shatter.png'),
+    u('images/special4.png'),
+    u('images/special5-lion.png'),
+    u('images/specialEffect1.png'),
+    u('images/x_R.png'),
+    u('images/top.png'),
+    u('images/topWithScore.png'),
+    u('images/trinity_object_glow.png'),
+    u('images/trinity_object_new.png'),
+    u('images/wadePowered.png'),
+    u('images/yt_R.png'),
+    u('sounds/Explosion3.aac'),
+    u('sounds/Explosion3.ogg'),
+    u('sounds/PowerUp8.aac'),
+    u('sounds/PowerUp8.ogg'),
+    u('sounds/Walperion-Music-Ode-to-Victory.aac'),
+    u('sounds/Walperion-Music-Ode-to-Victory.ogg'),
+    u('sounds/fiveSound-lion.aac'),
+    u('sounds/fiveSound-lion.ogg'),
+    u('sounds/fiveSound-lionAtWAR.ogg')
+  ];
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(filesToCache);
     })
   );
-  self.skipWaiting(); // Activate the service worker immediately after installation
+  self.skipWaiting();
 });
 
-// Activate event - Cleanup old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+self.addEventListener('activate', function (event) {
+  var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(keyList =>
-      Promise.all(
-        keyList.map(key => {
-          if (!cacheWhitelist.includes(key)) {
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (cacheWhitelist.indexOf(key) === -1) {
             return caches.delete(key);
           }
         })
-      )
-    )
+      );
+    })
   );
-  self.clients.claim(); // Take control of all clients immediately after activation
+  self.clients.claim();
 });
 
-// Fetch event - Serve cached content when offline, else fetch from network
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
 });
 
-// Listen for skipWaiting message from the client to update only when the user requests it
-self.addEventListener('message', event => {
+self.addEventListener('message', function (event) {
   if (event.data === 'skipWaiting') {
     self.skipWaiting();
   }
